3aa9d8d603d65f374bf8908d1317374ca6465001
lehuutien1202 <lehuutien.1202@gmail.com>
2022-06-06 14:37:22 +0700

    ad

---
+++ src/Bai1/Bai1.java
@@ -0,0 +1,10 @@
+package Bai1;
+
+import java.util.Date;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        Date now = new Date ();
+        System.out.println("Now is: " + now);
+    }
+}
