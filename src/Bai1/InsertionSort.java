35c930bac4c3d33e021589fcdb2b000e71bceb69
lehuutien1202 <lehuutien.1202@gmail.com>
2022-06-10 14:01:51 +0700

    kiemtrasonguyento

---
+++ src/Bai1/InsertionSort.java
@@ -0,0 +1,7 @@
+package Bai1;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Bai2/CheckIntegerElement.java
@@ -0,0 +1,30 @@
+package Bai2;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class CheckIntegerElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int number = sc.nextInt();
+        if (number < 2){
+            System.out.println(number + "is not a prime");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)){
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check) {
+                System.out.println(number + " is a prime");
+            } else
+                System.out.println(number + " is not a prime");
+        }
+
+    }
+}
